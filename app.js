a3c993f4143868f51be9584be3af43f316b3e526
ayecristi <ayelendcristi@gmail.com>
2026-04-18 16:10:29 -0300

    se agregan algunas configuraciones iniciales en app.js

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const PORT = 3000;
 // const personasRoutes = require("./routes/personasRoutes");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+
+app.set('view engine', 'pug');
+app.set('views', './views');
 // usar rutas
 // app.use("/personas", personasRoutes);
+
+
+app.get('/', (req, res) => {
+    res.send('¡Servidor de TechRetail funcionando perfecto!');
+});
 
 app.listen(PORT, () => {
     console.log("Servidor corriendo en puerto " + PORT);
