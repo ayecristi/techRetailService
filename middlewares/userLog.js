af916913003db2987e2ec3fa0cb3fe69cb23719e
ayecristi <ayelendcristi@gmail.com>
2026-04-24 13:18:17 -0300

    clean code

--- middlewares/userLog.js
+++ middlewares/userLog.js
@@ -1,11 +1,10 @@
 module.exports = (req, res, next) => {
-    const inicio = Date.now(); // Tomamos el tiempo de inicio
-    console.log(`[${new Date().toLocaleTimeString()}] 🟢 Recibida: ${req.method} en ${req.url}`);
+    const inicio = Date.now(); 
+    console.log(`[${new Date().toLocaleTimeString()}] Recibida: ${req.method} en ${req.url}`);
 
-    // Escuchamos cuando la respuesta termine de enviarse
     res.on('finish', () => {
         const duracion = Date.now() - inicio;
-        console.log(`[${new Date().toLocaleTimeString()}] 🏁 Finalizada: ${req.method} en ${req.url} - Status: ${res.statusCode} (${duracion}ms)`);
+        console.log(`[${new Date().toLocaleTimeString()}] Finalizada: ${req.method} en ${req.url} - Status: ${res.statusCode} (${duracion}ms)`);
     });
 
     next();
