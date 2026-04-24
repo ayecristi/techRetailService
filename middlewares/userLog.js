42351766373f7b94b8322c1eeb8d4ce14476dd3f
ayecristi <ayelendcristi@gmail.com>
2026-04-24 13:15:07 -0300

    fix: agregar monitoreo de finalización y status code en log de auditoría

--- middlewares/userLog.js
+++ middlewares/userLog.js
@@ -1,4 +1,12 @@
 module.exports = (req, res, next) => {
-    console.log(`[${new Date().toLocaleTimeString()}] Se recibió una petición ${req.method} en ${req.url}`);
+    const inicio = Date.now(); // Tomamos el tiempo de inicio
+    console.log(`[${new Date().toLocaleTimeString()}] 🟢 Recibida: ${req.method} en ${req.url}`);
+
+    // Escuchamos cuando la respuesta termine de enviarse
+    res.on('finish', () => {
+        const duracion = Date.now() - inicio;
+        console.log(`[${new Date().toLocaleTimeString()}] 🏁 Finalizada: ${req.method} en ${req.url} - Status: ${res.statusCode} (${duracion}ms)`);
+    });
+
     next();
 };
