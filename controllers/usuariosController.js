40b547fec03e5b4b81d93d8268dbfcec6ea25a8d
luciograma <lucio.gramaccioni@gmail.com>
2026-05-14 21:31:00 -0300

    Configuracion de DB local - Clean Files .idea

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -3,20 +3,29 @@ const path = require("path");
 const Usuario = require("../models/Usuario");
 const rutaArchivo = path.join(__dirname, "../data/usuarios.json");
 
+
+// leer archivo
 const leerUsuarios = () => {
     const data = fs.readFileSync(rutaArchivo, "utf-8");
     return JSON.parse(data);
+
 };
 
+// guardar archivo
 const guardarUsuarios = (usuarios) => {
-    fs.writeFileSync(rutaArchivo, JSON.stringify(usuarios, null, 2));
+    fs.writeFileSync(
+        rutaArchivo,
+        JSON.stringify(usuarios, null, 2)
+    );
+
 };
 
+// GET ALL
 const obtenerUsuarios = (req, res) => {
     const usuarios = leerUsuarios();
     res.json(usuarios);
+    
 };
-
 const obtenerUsuariosVista = (req, res) => {
     try {
         const usuarios = leerUsuarios();
@@ -27,56 +36,45 @@ const obtenerUsuariosVista = (req, res) => {
     }
 };
 
+// GET BY ID
 const obtenerUsuarioPorId = (req, res) => {
     const usuarios = leerUsuarios();
     const id = parseInt(req.params.id);
     const usuario = usuarios.find(u => u.id === id);
-
     if (!usuario) {
         return res.status(404).json({
             mensaje: "Usuario no encontrado"
         });
     }
-
     res.json(usuario);
 };
 
+
+// CREATE
 const crearUsuario = (req, res) => {
     const usuarios = leerUsuarios();
     const { id, nombre, email, rol, telefono, password, tiendaId, activo, fechaCreacion } = req.body;
-
-    const nuevoId = id ? Number(id) : (usuarios.length > 0 ? Math.max(...usuarios.map(u => u.id)) + 1 : 1);
-    const nuevoUsuario = new Usuario(
-        nuevoId,
-        nombre,
-        email,
-        rol,
-        telefono,
-        password,
-        tiendaId,
-        activo,
-        fechaCreacion
-    );
-
+    const nuevoUsuario = new Usuario(Number(id), nombre, email, rol, telefono, password, tiendaId, activo, fechaCreacion);
     usuarios.push(nuevoUsuario);
     guardarUsuarios(usuarios);
-
     if (req.originalUrl.includes('/api')) {
-        return res.status(201).json(nuevoUsuario);
-    }
-
-    return res.redirect('/usuarios/listar');
+            // Si llama a la API (Thunder Client), devuelvo JSON
+            return res.status(201).json(nuevoUsuario);
+        } else {
+            // Si llamaron desde el formulario web (/guardar), redirijo a la lista
+            return res.redirect('/usuarios/listar');
+        }
 };
 
 const formularioNuevoUsuario = (req, res) => {
     res.render("usuarios/nuevo");
 };
 
+// UPDATE
 const actualizarUsuario = (req, res) => {
     const usuarios = leerUsuarios();
     const id = parseInt(req.params.id);
     const usuario = usuarios.find(u => u.id === id);
-
     if (!usuario) {
         if (req.originalUrl.includes('/api')) {
             return res.status(404).json({ mensaje: "Usuario no encontrado" });
@@ -96,14 +94,14 @@ const actualizarUsuario = (req, res) => {
 
     guardarUsuarios(usuarios);
 
-    if (req.originalUrl.includes('/api')) {
+   if (req.originalUrl.includes('/api')) {
         return res.status(200).json({
             mensaje: "Usuario actualizado",
             usuario
         });
+    } else {
+        return res.redirect('/usuarios/listar');
     }
-
-    return res.redirect('/usuarios/listar');
 };
 
 const formularioEditarUsuario = (req, res) => {
@@ -115,30 +113,37 @@ const formularioEditarUsuario = (req, res) => {
         return res.status(404).send("Usuario no encontrado para editar");
     }
 
-    res.render("usuarios/editar", { usuario });
+    res.render("usuarios/editar", { usuario: usuario });
 };
 
+
+// DELETE
 const eliminarUsuario = (req, res) => {
     const usuarios = leerUsuarios();
     const id = parseInt(req.params.id);
     const nuevosUsuarios = usuarios.filter(u => u.id !== id);
-
     if (usuarios.length === nuevosUsuarios.length) {
         if (req.originalUrl.includes('/api')) {
-            return res.status(404).json({ mensaje: "Usuario no encontrado" });
+            return res.status(404).json({
+                mensaje: "Usuario no encontrado"
+            });
         } else {
             return res.status(404).send("Usuario no encontrado");
         }
     }
-
     guardarUsuarios(nuevosUsuarios);
 
+    //actualizar vista
     if (req.originalUrl.includes('/api')) {
-        return res.status(200).json({ mensaje: "Usuario eliminado" });
+        return res.status(200).json({
+            mensaje: "Usuario eliminado"
+        });
+    } else {
+        return res.redirect("/usuarios/listar");
     }
 
-    return res.redirect("/usuarios/listar");
 };
+
 
 module.exports = {
     obtenerUsuarios,
